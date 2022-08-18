32eaa0605bf0467aa3ae883630b021888f726454
ItzThatKidBrian <sampsonbrian863@gmail.com>
2022-08-18 15:26:46 -0400

    Added a simple GUI

--- GUI.py
+++ GUI.py
@@ -1,10 +1,11 @@
+from cgitb import enable
 from tkinter import *
-from WebScraper import scrapeStockPrice
-import itertools
-#import pandas as pd
+from WebScraper import getStockPriceDict, scrapeStockPrice
 
-root = Tk(className="Python WebScraper")
-root.geometry("700x700")
+global counter
+root = Tk(className="StockWebScraper")
+root.geometry("300x200")
+root.resizable(width=False, height=False)
 
 def clickFunc():
     stock = entry.get()
@@ -12,21 +13,28 @@ def clickFunc():
     if(stock != ""):
         global newLabel
         stockPrice = scrapeStockPrice(stock)
-        newLabel = Label(root, text="The price of " + stock + " is $" + stockPrice)
+        if(stockPrice == "-1"):
+            newLabel = Label(root, text="INVALID STOCK TICKER.", font=("Courier", 12))
+            newLabel.pack()
+            newButton['state'] = DISABLED
+            return
+        newLabel = Label(root, text="The price of " + stock + " is $" + stockPrice, font=("Courier", 12))
         newLabel.pack()
+        newButton['state'] = DISABLED
 
 def deleteStock():
     newLabel.destroy()
+    newButton['state'] = ACTIVE
     return
 
 entry = Entry(root, width="30")
+entry.insert(0, "Ex. AMD, MSFT, SPY...")
 entry.pack()
 
-newButton = Button(root, text="Add a stock", pady="40", padx="100", command=clickFunc)
+newButton = Button(root, text="Add stock ticker", pady="20", padx="30", command=clickFunc)
 newButton.pack()
 
-deleteButton = Button(root, text="Delete", command=deleteStock)
-deleteButton.pack(pady="10")
-
+deleteButton = Button(root, text="Remove stock ticker", pady="20", padx="30", command=deleteStock)
+deleteButton.pack()
 
 root.mainloop()
